724558788939a508a684fa214a85da36e85a7261
Dean27817 <79397523+Dean27817@users.noreply.github.com>
2024-01-29 10:03:23 -0700

    Add files via upload

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -20,6 +20,70 @@ void Robot::TeleopPeriodic()
   Robot::arm();
 }
 
+//takes the joystick input and sets the direction sent to the drive function
+void Robot::direction()
+{
+  float forward = m_joystick.GetRawAxis(0);
+  float rotation = m_joystick.GetRawAxis(1);
+  if (!m_joystick.GetRawButton(2))
+  {
+    rotation /= 1.5;
+  }
+  if (m_joystick.GetRawButton(3))
+  {
+    rotation *= 2;
+  }
+  // std::cout << "left motor " << forward-rotation << "\nright motor " <<
+  // forward + rotation << "\n";
+  if (forward > 0.2 || forward < -0.2 || rotation > 0.2 || rotation < -0.2)
+  {
+    drive(forward - rotation, forward + rotation);
+  }
+  else
+  {
+    drive(0, 0);
+  }
+}
+
+//controlls the arm of the robot, currently just for the shooting mechanisum from an xbox controller
+//left index trigger to shoot and right index trigger for intake
+//the index trigger is the small button that your index finger rests on while holding the throttle
+void Robot::arm()
+{
+  if(m_joystick.GetRawButton(20))
+  {
+    m_intakeL.Set(ControlMode::PercentOutput, 0.2);
+    m_intakeR.Set(ControlMode::PercentOutput, -0.2);
+  }
+  if(m_joystick.GetRawButton(22))
+  {
+    m_intakeL.Set(ControlMode::PercentOutput, -intakeSpeed);
+    m_intakeR.Set(ControlMode::PercentOutput, intakeSpeed);
+  }
+  if(!m_joystick.GetRawButton(20) && !m_joystick.GetRawButton(22))
+  {
+    m_intakeL.Set(ControlMode::PercentOutput, 0);
+    m_intakeR.Set(ControlMode::PercentOutput, 0);
+  }
+}
+
+//sets the motors on either ide to the variables that are sent to it
+void Robot::drive(double left, double right)
+{
+  // setting motor speeds to parameters
+  left *= (((-m_joystick.GetRawAxis(4)) + 1) / 2) +
+          (0.2 * (1 - m_joystick.GetRawAxis(4)));
+  right *= (((-m_joystick.GetRawAxis(4)) + 1) / 2);
+  // if (m_pdp.GetCurrent(14) < 10 && m_pdp.GetCurrent(15) < 10 &&
+  // m_pdp.GetCurrent(16) < 10 && m_pdp.GetCurrent(17) < 10)
+  {
+    m_driveMotorLBack.Set(ControlMode::PercentOutput, left);
+    m_driveMotorLFront.Set(ControlMode::PercentOutput, left);
+    m_driveMotorRBack.Set(ControlMode::PercentOutput, right);
+    m_driveMotorRFront.Set(ControlMode::PercentOutput, right);
+  }
+}
+
 void Robot::DisabledInit() {}
 void Robot::DisabledPeriodic() {}
 
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -30,71 +30,13 @@ class Robot : public frc::TimedRobot {
   void SimulationInit() override;
   void SimulationPeriodic() override;
 
-  double intakeSpeed = 0.5;
-void direction(){
-  float forward = m_joystick.GetRawAxis(0);
-  float rotation = m_joystick.GetRawAxis(1);
-  if (!m_joystick.GetRawButton(2))
-  {
-    rotation /= 1.5;
-  }
-  if (m_joystick.GetRawButton(3))
-  {
-    rotation *= 2;
-  }
-  // std::cout << "left motor " << forward-rotation << "\nright motor " <<
-  // forward + rotation << "\n";
-  if (forward > 0.2 || forward < -0.2 || rotation > 0.2 || rotation < -0.2)
-  {
-    drive(forward - rotation, forward + rotation);
-  }
-  else
-  {
-    drive(0, 0);
-  }
-}
+  double intakeSpeed = 1;
+void direction();
 
-  void drive(double left, double right)
-{
-  // setting motor speeds to parameters
-  left *= -(((-m_joystick.GetRawAxis(4)) + 1) / 2) +
-           (0.2 * (1 - m_joystick.GetRawAxis(4)));
-  right *= (((-m_joystick.GetRawAxis(4)) + 1) / 2);
-  // if (m_pdp.GetCurrent(14) < 10 && m_pdp.GetCurrent(15) < 10 &&
-  // m_pdp.GetCurrent(16) < 10 && m_pdp.GetCurrent(17) < 10)
-  {
-    m_driveMotorLBack.Set(ControlMode::PercentOutput, left);
-    m_driveMotorLFront.Set(ControlMode::PercentOutput, left);
-    m_driveMotorRBack.Set(ControlMode::PercentOutput, right);
-    m_driveMotorRFront.Set(ControlMode::PercentOutput, right);
-  }
-}
+void drive(double left, double right);
 
-void arm()
-{
-  if(m_armControll.GetLeftBumper())
-  {
-    m_intakeL.Set(ControlMode::PercentOutput, intakeSpeed);
-    m_intakeR.Set(ControlMode::PercentOutput, -intakeSpeed);
-  }
-  if(m_armControll.GetRightBumper())
-  {
-    m_intakeL.Set(ControlMode::PercentOutput, -intakeSpeed);
-    m_intakeR.Set(ControlMode::PercentOutput, intakeSpeed);
-  }
-  if(m_armControll.GetXButton())
-  {
-    intakeSpeed = 0.5;
-  }
-  if(m_armControll.GetYButton())
-  {
-    intakeSpeed = 0.75;
-  }
-  if(m_armControll.GetBButton())
-  {
-    intakeSpeed = 0.1;
-  }
-}
+void arm();
+
 
   private:
   TalonFX m_driveMotorRFront{2};
@@ -104,5 +46,5 @@ void arm()
   TalonFX m_intakeL{16};
   TalonFX m_intakeR{15};
   frc::Joystick m_joystick{0};
-  frc::XboxController m_armControll{1};
+  //frc::XboxController m_armControll{1};
 };
